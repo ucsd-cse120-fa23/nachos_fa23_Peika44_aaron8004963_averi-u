710177c460b0121604691fe32fb7978a7cc5e2ea
Peike Xu <p3xu@ucsd.edu>
2023-10-25 13:17:31 -0700

    seems to work

--- nachos/threads/Rendezvous.java
+++ nachos/threads/Rendezvous.java
@@ -5,6 +5,7 @@ package nachos.threads;
 
 import nachos.machine.*;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.HashMap;
 
 /**
@@ -13,72 +14,113 @@ import java.util.HashMap;
 public class Rendezvous {
 
     private static Lock lock;
-    private static Condition cv;
+    private static Condition notEmpty;
+    private static Condition notFull;
     private int instanceValue;
     private Rendezvoustag tags;
 
-    private class Pair<K, V> {
-        private K key;
-        private V value;
+    /*
+     * version with integer and pair of integer
+     * store data, value, and value
+     */
+    // private class Pair<K, V> {
+    //     private K key;
+    //     private V value;
     
-        public Pair(K key, V value) {
-            this.key = key;
-            this.value = value;
-        }
+    //     public Pair(K key, V value) {
+    //         this.key = key;
+    //         this.value = value;
+    //     }
     
-        public K getKey() {
-            return key;
-        }
+    //     public K getKey() {
+    //         return key;
+    //     }
     
-        public V getValue() {
-            return value;
-        }
+    //     public V getValue() {
+    //         return value;
+    //     }
 
-        public void setValue(V value){
-            this.value = value;
-        }
+    //     public void setValue(V value){
+    //         this.value = value;
+    //     }
 
-    }
+    // }
 
-    private class Rendezvoustag{
-        // private Map<Integer, Pair<Integer, Condition>> tags;
-        // //private int value;
+    // private class Rendezvoustag{
+    //     // private Map<Integer, Pair<Integer, Condition>> tags;
+    //     // //private int value;
+
+    //     Rendezvoustag(){
+    //         //tags = new HashMap<Integer, Pair<Integer, Condition>>();
+    //         //this.value = value;
+    //         tags = new HashMap<Integer, Pair<Integer,Integer>>();
+    //     }
+        
+    //     // public Map<Integer, Pair<Integer, Condition>> getTags(){
+    //     //     return this.tags;
+    //     // }
+
+    //     private Map<Integer, Pair<Integer,Integer>> tags;
+        
+    //     // public void addTag(int tag, int value, Condition cv ){
+    //     //     this.tags.put(tag, new Pair<Integer, Condition>(value, cv));
+    //     // }
+
+    //     public Map<Integer, Pair<Integer,Integer>> getTags(){
+    //         return this.tags;
+    //     }
+
+    //     public void addTag(int tag, int value, int lastValue){
+    //         this.tags.put(tag, new Pair<Integer, Integer>(value, lastValue));
+    //     }
+
+    //     public void deleteTag(int tag){
+    //         this.tags.remove(tag);
+    //     }
+    // }
+
+    /*
+     * new version tried to block other threads
+     * until the cooresponding thread finished
+     */
+
+     private class Rendezvoustag{
+        private Map<Integer,Integer> tags;
 
         Rendezvoustag(){
-            //tags = new HashMap<Integer, Pair<Integer, Condition>>();
-            //this.value = value;
-            tags = new HashMap<Integer, Pair<Integer,Integer>>();
+            tags = new ConcurrentHashMap<Integer,Integer>();
         }
-        
-        // public Map<Integer, Pair<Integer, Condition>> getTags(){
+
+        // public Map<Integer,Integer> getTags(){
         //     return this.tags;
         // }
 
-        private Map<Integer, Pair<Integer,Integer>> tags;
-        
-        // public void addTag(int tag, int value, Condition cv ){
-        //     this.tags.put(tag, new Pair<Integer, Condition>(value, cv));
-        // }
-
-        public Map<Integer, Pair<Integer,Integer>> getTags(){
-            return this.tags;
+        public int getValue(int tag){
+            return this.tags.get(tag);
         }
 
-        public void addTag(int tag, int value, int lastValue){
-            this.tags.put(tag, new Pair<Integer, Integer>(value, lastValue));
+        public void addTag(int tag, int value){
+            this.tags.put(tag, value);
         }
 
         public void deleteTag(int tag){
             this.tags.remove(tag);
         }
-    }
+
+        public boolean containsTag(int tag){
+            return this.tags.containsKey(tag);
+        }
+
+     }
     /**
      * Allocate a new Rendezvous.
      */
     public Rendezvous () {
         lock = new Lock();
-        cv = new Condition(lock);
+        notEmpty = new Condition(lock);
+        notFull = new Condition(lock);
         tags = new Rendezvoustag();
+        int i = 0;
     }
 
     /**
@@ -100,43 +142,72 @@ public class Rendezvous {
     public int exchange (int tag, int value) {
         lock.acquire();
         try{
-            if(tags.getTags().containsKey(tag)){
-                //int v = tags.getTags().get(tag).getKey();
+            // if(tags.getTags().containsKey(tag)){
                 
-                //cv = tags.getTags().get(tag).getValue();
-                
-                int v = tags.getTags().get(tag).getKey();
-
-
-                tags.getTags().get(tag).setValue(value);
-
-                //tags.deleteTag(tag);
-                System.out.println( KThread.currentThread().getName());
-                cv.wake();
-                System.out.println('0');
+            //     int v = tags.getTags().get(tag).getKey();
+            //     tags.getTags().get(tag).setValue(value);
+            //     System.out.println( KThread.currentThread().getName());
+            //     cv.wake();
+            //     System.out.println('0');
+            //     return v;
+            // }
+            // else{
+            //     tags.addTag(tag, value, value);
+            //     System.out.println('2');
+            //     cv.sleep();
+            //     System.out.println('1');
+            //     int instance = tags.getTags().get(tag).getValue();
+            //     tags.deleteTag(tag);
+            //     return instance;  
+            // }
+                // if(tags.containsTag(tag)){
+                //     int v = tags.getValue(tag);
+                //     tags.addTag(tag, value);
+                //     notEmpty.wake();
+                //     return v;
+                // }
+                // else{
+                //     tags.addTag(tag, value);
+                //     notFull.sleep();
+                //     int instance = tags.getValue(tag);
+                //     tags.deleteTag(tag);
+                //     return instance;
+                // }
+            
+            
+            if(tags.containsTag(tag)){
+                int v = get_tag(tag);
+                put_tag(tag, value);
                 return v;
             }
             else{
-                // cv = new Condition(lock);
-                // tags.addTag(tag, value, cv);
-                
-
-                //instanceValue = value;
-
-                tags.addTag(tag, value, value);
-
-                System.out.println('2');
-                cv.sleep();
-                System.out.println('1');
-                int instance = tags.getTags().get(tag).getValue();
-                tags.deleteTag(tag);
-                return instance;  
+                put_tag(tag, value);
+                notFull.sleep();
+                return get_tag(tag);
             }
-
+            
         }
         finally{
             lock.release();
         }
+    }
+
+    private void put_tag(int tag, int value){
+        while(tags.containsTag(tag)){
+            notFull.sleep();
+        }
+        tags.addTag(tag, value);
+        notEmpty.wake();
+    }
+
+    private int get_tag(int tag){
+        while(!tags.containsTag(tag)){
+            notEmpty.sleep();
+        }
+        int instance = tags.getValue(tag);
+        tags.deleteTag(tag);
+        notFull.wake();
+        return instance;
     }
     
     // Place Rendezvous test code inside of the Rendezvous class.
@@ -185,7 +256,7 @@ public class Rendezvous {
 
                 System.out.println ("Thread " + KThread.currentThread().getName() + " exchanging " + send);
                 int recv = r.exchange (tag, send);
-                //Lib.assertTrue (recv == 1, "Was expecting " + 1 + " but received " + recv);
+                Lib.assertTrue (recv == 1, "Was expecting " + 1 + " but received " + recv);
                 System.out.println ("Thread " + KThread.currentThread().getName() + " received " + recv + " with tag " + tag);
             }
         });
@@ -197,7 +268,7 @@ public class Rendezvous {
 
                 System.out.println ("Thread " + KThread.currentThread().getName() + " exchanging " + send);
                 int recv = r.exchange (tag, send);
-                //Lib.assertTrue (recv == 5, "Was expecting " + 5 + " but received " + recv);
+                Lib.assertTrue (recv == 5, "Was expecting " + 5 + " but received " + recv);
                 System.out.println ("Thread " + KThread.currentThread().getName() + " received " + recv + " with tag " + tag);
             }
         });
@@ -210,7 +281,7 @@ public class Rendezvous {
 
                 System.out.println ("Thread " + KThread.currentThread().getName() + " exchanging " + send);
                 int recv = r.exchange (tag, send);
-                //Lib.assertTrue (recv == -1, "Was expecting " + -1 + " but received " + recv);
+                Lib.assertTrue (recv == -1, "Was expecting " + -1 + " but received " + recv);
                 System.out.println ("Thread " + KThread.currentThread().getName() + " received " + recv + " with tag " + tag);
             }
         });
@@ -223,7 +294,7 @@ public class Rendezvous {
 
                 System.out.println ("Thread " + KThread.currentThread().getName() + " exchanging " + send);
                 int recv = r.exchange (tag, send);
-                //Lib.assertTrue (recv == 0, "Was expecting " + 0 + " but received " + recv);
+                Lib.assertTrue (recv == 0, "Was expecting " + 0 + " but received " + recv);
                 System.out.println ("Thread " + KThread.currentThread().getName() + " received " + recv + " with tag " + tag);
             }
         });
