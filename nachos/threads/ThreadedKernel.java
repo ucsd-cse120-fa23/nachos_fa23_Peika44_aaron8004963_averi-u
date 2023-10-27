52bc8745767a4be13102adacb742ee16807420a4
Aaron Li <Aaron8004963@gmail.com>
2023-10-27 16:48:54 -0700

    dos

--- nachos/threads/ThreadedKernel.java
+++ nachos/threads/ThreadedKernel.java
@@ -47,11 +47,11 @@ public class ThreadedKernel extends Kernel {
 	 */
 	public void selfTest() {
 		KThread.selfTest();
-		//Semaphore.selfTest();
-		//SynchList.selfTest();
-    	//Alarm.selfTest();
+		Semaphore.selfTest();
+		SynchList.selfTest();
+    	Alarm.selfTest();
 		Rendezvous.selfTest();
-		//Condition2.selfTest();
+		Condition2.selfTest();
 
 		if (Machine.bank() != null) {
 			ElevatorBank.selfTest();
