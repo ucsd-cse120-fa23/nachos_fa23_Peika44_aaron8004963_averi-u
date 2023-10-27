6894b39618ba33cd32a15067e7ab0a5f3ffa8510
Averi Yu / Zhizhen Yu <73741014+averi-u@users.noreply.github.com>
2023-10-27 16:28:19 -0700

    Update Condition2.java

--- nachos/threads/Condition2.java
+++ nachos/threads/Condition2.java
@@ -195,6 +195,21 @@ public class Condition2 {
 
     // Invoke Condition2.selfTest() from ThreadedKernel.selfTest()
 
+	private static void sleepForTest1 () {
+		Lock lock = new Lock();
+		Condition2 cv = new Condition2(lock);
+	
+		lock.acquire();
+		long t0 = Machine.timer().getTime();
+		System.out.println (KThread.currentThread().getName() + " sleeping");
+		// no other thread will wake us up, so we should time out
+		cv.sleepFor(2000);
+		long t1 = Machine.timer().getTime();
+		System.out.println (KThread.currentThread().getName() +
+					" woke up, slept for " + (t1 - t0) + " ticks");
+		lock.release();
+	}
+
     public static void selfTest() {
 		System.out.println("\n" +
 			"-----------------------------InterlockTest()---------------------------------------"
@@ -204,7 +219,12 @@ public class Condition2 {
 			"-----------------------------InterlockTest2()---------------------------------------"
 			);
 		new InterlockTest2();
+		System.out.println("\n" +
+			"-----------------------------sleepForTest1()---------------------------------------"
+			);
+		sleepForTest1();
     }
         private Lock conditionLock;
 		private LinkedList<KThread> waitQueue;
+		
 }
