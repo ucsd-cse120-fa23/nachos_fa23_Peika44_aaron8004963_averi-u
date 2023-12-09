99c12e45499b2fa125280d21beb9df6be5000ba0
averi-u <zhy008@ucsd.edu>
2023-12-08 17:39:43 -0800

    n

--- nachos/vm/VMKernel.java
+++ nachos/vm/VMKernel.java
@@ -1,87 +1,3 @@
-// package nachos.vm;
-
-// import java.util.LinkedList;
-
-// import nachos.machine.*;
-// import nachos.threads.*;
-// import nachos.userprog.*;
-// import nachos.vm.*;
-
-// /**
-//  * A kernel that can support multiple demand-paging user processes.
-//  */
-// public class VMKernel extends UserKernel {
-// 	/**
-// 	 * Allocate a new VM kernel.
-// 	 */
-// 	public VMKernel() {
-// 		super();
-// 	}
-
-// 	/**
-// 	 * Initialize this kernel.
-// 	 */
-// 	public void initialize(String[] args) {
-// 		super.initialize(args);
-// 	}
-
-// 	/**
-// 	 * Test this kernel.
-// 	 */
-// 	public void selfTest() {
-// 		super.selfTest();
-// 	}
-
-// 	/**
-// 	 * Start running user programs.
-// 	 */
-// 	public void run() {
-// 		super.run();
-// 	}
-
-// 	/**
-// 	 * Terminate this kernel. Never returns.
-// 	 */
-// 	public void terminate() {
-// 		super.terminate();
-// 	}
-
-// 	// dummy variables to make javac smarter
-// 	private static VMProcess dummy1 = null;
-
-// 	private static final char dbgVM = 'v';
-// 	//starter codes end
-
-// 	public static int victimIndex;
-
-// 	public static System IPT[];
-
-// 	public static LinkedList<Integer> availableSwapPages;
-
-// 	public static OpenFile swap;
-
-// 	public static int sp;
-
-// 	public static Lock mutex;
-
-// 	public static Condition CV;
-
-// 	public static int numPin;
-
-// 	protected class System{
-// 		public VMProcess process;
-// 		public TranslationEntry entry;
-// 		public boolean pin;
-
-// 		public System(VMProcess process, TranslationEntry entry, boolean pin){
-// 		  this.process = process;
-// 		  this.entry = entry;
-// 		  this.pin = pin;
-// 		}           
-// 	  }
-
-// }
-
 package nachos.vm;
 
 import nachos.machine.*;
@@ -144,6 +60,7 @@ public class VMKernel extends UserKernel {
 		super.terminate();
 	}
 
+	
 	protected class PageInfo {
 		public VMProcess process;
 		public TranslationEntry entry;
